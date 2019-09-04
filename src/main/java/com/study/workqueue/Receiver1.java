eb083b9590bb3e1718940ff2a92d2206fc303a59
Joey <woqiaoxun@163.com>
2019-09-04 22:00:25 +0800

    workqueue

---
+++ src/main/java/com/study/workqueue/Receiver1.java
@@ -0,0 +1,37 @@
+package com.study.workqueue;
+
+import com.rabbitmq.client.*;
+import com.study.utils.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver1 {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        newApi();
+    }
+
+    public static void newApi() throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body)
+                    throws IOException {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("message 1: " + new String(body));
+            }
+        };
+
+        // 自动应答
+        boolean autoAck = true;
+        channel.basicConsume(Sender.workQueue, autoAck, defaultConsumer);
+    }
+}

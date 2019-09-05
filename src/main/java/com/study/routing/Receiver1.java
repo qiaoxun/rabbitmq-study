5edeaeeb7c8336b28d5a6deb84cf3ceee21d5d42
Joey <woqiaoxun@163.com>
2019-09-05 22:12:15 +0800

    routing

---
+++ src/main/java/com/study/routing/Receiver1.java
@@ -0,0 +1,48 @@
+package com.study.routing;
+
+import com.rabbitmq.client.*;
+import com.study.utils.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver1 {
+    public final static String QUEUE_NAME = "publish_routing_queue1";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        channel.queueBind(QUEUE_NAME, Sender.EXCHANGE_NAME, "error");
+
+        /**
+         * 每个消费者发送确认信息之前，消息队列不发送下一个消息到消费者，一次只处理一个消息
+         * 限制发送给同一个消费者不得超过一条信息
+         */
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
+
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body)
+                    throws IOException {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    // 手动回执完成消息
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+                System.out.println("message 1: " + new String(body));
+            }
+        };
+
+        // 自动应答
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, defaultConsumer);
+    }
+}
---
+++ src/main/java/com/study/routing/Sender.java
@@ -0,0 +1,28 @@
+package com.study.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.study.utils.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+    public final static String EXCHANGE_NAME = "test_exchange_routing";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+
+        // declare exchange
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        String msg = "hello routing - info";
+        channel.basicPublish(EXCHANGE_NAME, "info", null, msg.getBytes());
+
+        System.out.println("sent msg: " + msg);
+
+        channel.close();
+        connection.close();
+    }
+}

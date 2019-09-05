0d7602f825ee83d03e8d94b19e516663a1743e3a
Joey <woqiaoxun@163.com>
2019-09-05 23:01:55 +0800

    transaction

---
+++ src/main/java/com/study/tx/Sender.java
@@ -0,0 +1,38 @@
+package com.study.tx;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.study.utils.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+    public final static String EXCHANGE_NAME = "test_exchange_tx";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+
+        // declare exchange
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+        String msg = "hello routing - Goods.spoon.using";
+
+        try {
+            channel.txSelect();
+            channel.basicPublish(EXCHANGE_NAME, "Goods.spoon.using", null, msg.getBytes());
+            int i = 1/0;
+            channel.txCommit();
+        } catch (Exception e) {
+            channel.txRollback();
+            e.printStackTrace();
+        }
+
+        System.out.println("sent msg: " + msg);
+
+        channel.close();
+        connection.close();
+
+    }
+}
